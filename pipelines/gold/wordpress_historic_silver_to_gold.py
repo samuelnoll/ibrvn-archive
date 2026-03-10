1fa034c34233a426d850fe65811ee7774b5b5712
Samuel Noll <samuelnoll96@gmail.com>
2026-03-10 20:52:44 -0300

    Fixing all problems

--- pipelines/gold/wordpress_historic_silver_to_gold.py
+++ pipelines/gold/wordpress_historic_silver_to_gold.py
@@ -5,7 +5,7 @@ import unicodedata
 import yaml
 
 CSV_PATH = "data/silver/wordpress_sermons_clean.csv"
-DB_PATH = "data/gold/sermons.db"
+DB_PATH = "data/gold/wordpress_sermons.db"
 
 def load_preacher_map():
 
